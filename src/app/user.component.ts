e41e8a5406af1059fdb8bbb77ed0c604180730c1
Adrian Grillo <Adrian.Grillo@caemilusa.com>
2021-11-18 16:13:02 -0500

    resetting from past commit

--- src/app/user.component.ts
+++ src/app/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input} from "@angular/core";
+import { Component} from "@angular/core";
 
 @Component({
   selector: 'app-user',
@@ -11,5 +11,5 @@ import { Component, Input} from "@angular/core";
 })
 
 export  class UserComponent {
-  @Input() name = 'Adrian'
+  name = 'Adrian'
 }
